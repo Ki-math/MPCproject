64e9b11c45096ffc5758670b9736392e4aaace5d
kohei <kohei@192.168.2.103>
2021-06-22 10:17:27 +0900

    最新バージョンコミット

--- util/initMaskNMPC.m
+++ util/initMaskNMPC.m
@@ -56,13 +56,13 @@ if strcmp(kindMPC,'SQP')
     if ~isfield(params, 'IterationMax')
         params.IterationMax = 50;
     end
-    if ~isfield(params, 'stateFunction')
-        error('Error:Please set the State function');
-    end
-    if ~isfield(params, 'measurementFunction')
-        error('Error:Please set the Measurement function');
-    end
-else
+%     if ~isfield(params, 'stateFunction')
+%         error('Error:Please set the State function');
+%     end
+%     if ~isfield(params, 'measurementFunction')
+%         error('Error:Please set the Measurement function');
+%     end
+elseif strcmp(kindMPC,'CGMRES')
     % CGMRES
     if ~isfield(params, 'Q')
         params.Q = ones(nx,1);
@@ -74,7 +74,7 @@ else
         params.R = ones(nu,1);
     end
     if ~isfield(params, 'PredictiveTime')
-        params.PredictiveHorizon = 1;
+        params.PredictiveTime = 1;
     end
     if ~isfield(params, 'divideNumber')
         params.divideNumber = 10;
@@ -100,22 +100,76 @@ else
     if ~isfield(params, 'IterationMax')
         params.IterationMax = nu*params.divideNumber;
     end
-    if ~isfield(params, 'stateFunction')
-        error('Error:Please set the State function');
-    end
-    if ~isfield(params, 'jacobianTerminalFunction')
-        error('Error:Please set the Jacobian about Terminal function');
-    end
-    if ~isfield(params, 'jacobianHamiltonianState')
-        error('Error:Please set the Jacobian of state about Hamiltonian');
-    end
-    if ~isfield(params, 'jacobianHamiltonianInput')
-        error('Error:Please set the Jacobian of Input about Hamiltonian');
-    end
+%     if ~isfield(params, 'stateFunction')
+%         error('Error:Please set the State function');
+%     end
+%     if ~isfield(params, 'jacobianTerminalFunction')
+%         error('Error:Please set the Jacobian about Terminal function');
+%     end
+%     if ~isfield(params, 'jacobianHamiltonianState')
+%         error('Error:Please set the Jacobian of state about Hamiltonian');
+%     end
+%     if ~isfield(params, 'jacobianHamiltonianInput')
+%         error('Error:Please set the Jacobian of Input about Hamiltonian');
+%     end
     
     temp = c2d(ss(tf(params.PredictiveTime,[(1/params.raiseGain),1])),Ts);
     params.T_a = temp.a;                         % Discrete system of the predictive time
     params.T_b = temp.b;                         % Discrete system of the predictive time
+else
+    % Newton-GMRES
+    if ~isfield(params, 'Q')
+        params.Q = ones(nx,1);
+    end
+    if ~isfield(params, 'Qf')
+        params.Qf = ones(nx,1);
+    end
+    if ~isfield(params, 'R')
+        params.R = ones(nu,1);
+    end
+    if ~isfield(params, 'PredictiveTime')
+        params.PredictiveTime = 1;
+    end
+    if ~isfield(params, 'divideNumber')
+        params.divideNumber = 10;
+    end
+    if ~isfield(params, 'raiseGain')
+        params.raiseGain = 1;
+    end
+    if ~isfield(params, 'MV_max')
+        params.MV_max = 999*ones(nu,1);
+    end
+    if ~isfield(params, 'MV_min')
+        params.MV_min = -999*ones(nu,1);
+    end
+    if ~isfield(params, 'h')
+        params.h = 0.001;
+    end
+    if ~isfield(params, 'initialConditionInputs')
+        params.initialConditionInputs = zeros(nu+ns+nL,1);
+    end
+    if ~isfield(params, 'InnerIterationMax')
+        params.InnerIterationMax = nu*params.divideNumber;
+    end
+    if ~isfield(params, 'OuterIterationMax')
+        params.OuterIterationMax = 5;
+    end
+%     if ~isfield(params, 'stateFunction')
+%         error('Error:Please set the State function');
+%     end
+%     if ~isfield(params, 'jacobianTerminalFunction')
+%         error('Error:Please set the Jacobian about Terminal function');
+%     end
+%     if ~isfield(params, 'jacobianHamiltonianState')
+%         error('Error:Please set the Jacobian of state about Hamiltonian');
+%     end
+%     if ~isfield(params, 'jacobianHamiltonianInput')
+%         error('Error:Please set the Jacobian of Input about Hamiltonian');
+%     end
+    
+    temp = c2d(ss(tf(params.PredictiveTime,[(1/params.raiseGain),1])),Ts);
+    params.T_a = temp.a;                         % Discrete system of the predictive time
+    params.T_b = temp.b;                         % Discrete system of the predictive time    
 end
 
 end
--- util/initMaskPortNMPC.m
+++ util/initMaskPortNMPC.m
@@ -1,4 +1,4 @@
-function nd = initMaskPortNMPC(nd,enableDist,enableSlack,enableStatus,kindNMPC)
+function nd = initMaskPortNMPC(nd,enableDist,enableSlack,enableStatus,enableYseq,enableFnorm,kindNMPC)
 
 % Mask Display
 if strcmp(kindNMPC,'SQP')
@@ -6,8 +6,13 @@ if strcmp(kindNMPC,'SQP')
          'port_label(''Output'', ' num2str(1) ', ''mv'')' char(10) ...
          'port_label(''Input'', ' num2str(1) ', ''state'')' char(10) ...
          'port_label(''Input'', ' num2str(2) ', ''ref'')'];
-else
+elseif strcmp(kindNMPC,'CGMRES')
     MaskDisplay = ['fprintf(''Nonlinear MPC\nC/GMRES'')' char(10) ...
+         'port_label(''Output'', ' num2str(1) ', ''mv'')' char(10) ...
+         'port_label(''Input'', ' num2str(1) ', ''state'')' char(10) ...
+         'port_label(''Input'', ' num2str(2) ', ''ref'')'];
+else
+    MaskDisplay = ['fprintf(''Nonlinear MPC\nN/GMRES'')' char(10) ...
          'port_label(''Output'', ' num2str(1) ', ''mv'')' char(10) ...
          'port_label(''Input'', ' num2str(1) ', ''state'')' char(10) ...
          'port_label(''Input'', ' num2str(2) ', ''ref'')'];
@@ -158,11 +163,94 @@ else
         catch
         end
     end
+    
+    % For Sequence Output
+    if ~enableYseq
+        % Disable the port
+        try
+            lh = get_param([gcb,'/yseq'],'LineHandles');
+            position = get_param([gcb, '/yseq'],'Position');
+            delete_line(lh.Inport(1));
+            delete_block([gcb, '/yseq']);
+            h = add_block('built-in/Terminator',[gcb,'/Terminator_yseq']);
+            set_param(h,'position',position);
+            ph1 = get_param(h,'PortHandles');
+            ph2 = get_param([gcb,'/Signal Specification yseq'],'PortHandles');
+            add_line(gcb,ph2.Outport(1),ph1.Inport(1));
+        catch
+        end
+    else
+        % Enable the port
+        try
+            lh = get_param([gcb,'/Terminator_yseq'],'LineHandles');
+            position = get_param([gcb, '/Terminator_yseq'],'Position');
+            delete_line(lh.Inport(1));
+            delete_block([gcb,'/Terminator_yseq']);
+            h = add_block('built-in/Outport',[gcb,'/yseq']);
+            set_param(h,'position',position);
+            ph1 = get_param(h,'PortHandles');
+            ph2 = get_param([gcb,'/Signal Specification yseq'],'PortHandles');
+            add_line(gcb,ph2.Outport(1),ph1.Inport(1));
+        catch
+        end
+    end
 
+    % For Fnorm Output
+    if ~enableFnorm
+        % Disable the port
+        try
+            lh = get_param([gcb,'/F'],'LineHandles');
+            position = get_param([gcb, '/F'],'Position');
+            delete_line(lh.Inport(1));
+            delete_block([gcb, '/F']);
+            h = add_block('built-in/Terminator',[gcb,'/Terminator_F']);
+            set_param(h,'position',position);
+            ph1 = get_param(h,'PortHandles');
+            ph2 = get_param([gcb,'/Signal Specification F'],'PortHandles');
+            add_line(gcb,ph2.Outport(1),ph1.Inport(1));
+        catch
+        end
+    else
+        % Enable the port
+        try
+            lh = get_param([gcb,'/Terminator_F'],'LineHandles');
+            position = get_param([gcb, '/Terminator_F'],'Position');
+            delete_line(lh.Inport(1));
+            delete_block([gcb,'/Terminator_F']);
+            h = add_block('built-in/Outport',[gcb,'/F']);
+            set_param(h,'position',position);
+            ph1 = get_param(h,'PortHandles');
+            ph2 = get_param([gcb,'/Signal Specification F'],'PortHandles');
+            add_line(gcb,ph2.Outport(1),ph1.Inport(1));
+        catch
+        end
+    end
+    
     % Sort order of Outport
     if enableStatus
         set_param([gcb,'/Status'],'Port','2');
         MaskDisplay = [MaskDisplay char(10) 'port_label(''Output'', ' num2str(2) ', ''Status'')'];
+        if enableYseq
+            set_param([gcb,'/yseq'],'Port','3');
+            MaskDisplay = [MaskDisplay char(10) 'port_label(''Output'', ' num2str(3) ', ''yseq'')'];
+            if enableFnorm
+                set_param([gcb,'/F'],'Port','4');
+                MaskDisplay = [MaskDisplay char(10) 'port_label(''Output'', ' num2str(4) ', ''F'')'];
+            end
+        elseif enableFnorm
+                set_param([gcb,'/F'],'Port','3');
+                MaskDisplay = [MaskDisplay char(10) 'port_label(''Output'', ' num2str(3) ', ''F'')'];
+        end
+    elseif  enableYseq
+        set_param([gcb,'/yseq'],'Port','2');
+        MaskDisplay = [MaskDisplay char(10) 'port_label(''Output'', ' num2str(2) ', ''yseq'')'];
+        if enableFnorm
+            set_param([gcb,'/F'],'Port','3');
+            MaskDisplay = [MaskDisplay char(10) 'port_label(''Output'', ' num2str(3) ', ''F'')'];
+        end
+    elseif enableFnorm
+        set_param([gcb,'/F'],'Port','2');
+        MaskDisplay = [MaskDisplay char(10) 'port_label(''Output'', ' num2str(2) ', ''F'')'];
     end
 end
 
